de41001d2bfa5de3b1af401289874a38a1947163
thelacker <dyatlikos3000@ya.ru>
2016-05-27 21:53:41 +0300

    models.py

---
+++ models.py
@@ -0,0 +1,20 @@
+from peewee import *
+
+db = PostgresqlDatabase("bcards", user="bcarduser", password="123")
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class TelegramUser(BaseModel):
+    chat_id = IntegerField(unique=True)
+    first_name = CharField(null=True, default="")
+    address = CharField(null=True, default="")
+    phone = IntegerField(null=True)
+    deleted = BooleanField(default=False)
+    state = CharField(default='main')
+
+def initTables():
+    db.connect()
+    db.create_tables([TelegramUser], True)
