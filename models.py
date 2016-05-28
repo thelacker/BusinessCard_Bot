646fcbbf027326fe09b51359b954be1d4f2d4ef1
thelacker <dyatlikos3000@ya.ru>
2016-05-28 18:18:36 +0300

    Inline

--- models.py
+++ models.py
@@ -3,24 +3,19 @@ import os
 
 db = SqliteDatabase(os.path.join(os.path.dirname(os.path.realpath(__file__)),'telegrambot.db'))
 
-
 class BaseModel(Model):
     class Meta:
         database = db
 
-
 class TelegramUser(BaseModel):
     chat_id = IntegerField(unique=True)
-    name = CharField(default='', null=True)
-    post = CharField(default='', null=True)
-    email = CharField(default='', null=True)
-    phone = CharField(default='', null=True)
+    first_name = CharField(null=True, default="")
+    post = CharField(null=True, default="")
+    phone = CharField(null=True)
+    email = CharField(null=True, default="")
     deleted = BooleanField(default=False)
-    state = CharField(default='main')
-
+    state = CharField(default="main", null=False)
 
 def initTables():
     db.connect()
     db.create_tables([TelegramUser], True)
-
-initTables()
